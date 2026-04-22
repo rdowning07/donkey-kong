5a75e35e7cbcedcef0a35046f037f22db2b7b77e
Rob Downing <redowning@gmail.com>
2026-04-21 22:25:05 -0700

    wired in jump physics

--- player.py
+++ player.py
@@ -9,15 +9,24 @@ class Player:
 
         self.velocity_x = 0.0
         self.velocity_y = 0.0
+        self.on_ground = False
 
     def update(self, dt):
-        self.velocity_y += GRAVITY * dt
+        if self.on_ground:
+            self.velocity_y = 0.0
+        else:
+            self.velocity_y += GRAVITY * dt
         self.x += self.velocity_x * dt
         self.y += self.velocity_y * dt
 
         if self.y > SCREEN_HEIGHT:
             self.y = SCREEN_HEIGHT - self.size
             self.velocity_y = 0.0
+
+    def jump(self):
+        if self.on_ground:
+            self.velocity_y = -JUMP_SPEED
+            self.on_ground = False
     
     def draw(self, screen):
         pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, self.size, self.size))
